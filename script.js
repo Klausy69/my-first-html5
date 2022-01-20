af1a8e0474496776e956a69732bba00cdfef4116
Klausy69 <98019818+Klausy69@users.noreply.github.com>
2022-01-20 15:09:47 +0530

    Add Button
    added a new button that leads to my spotify account.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let button1 = document.getElementById("ringo");
 let button2 = document.getElementById("ringo2");
+let button3 = document.getElementById("ringo3");
 
 function buttonClick() {
     window.open("https://krunker.io", "_blank");
@@ -9,7 +10,11 @@ function buttonClick2() {
     window.open("https://discord.com/login", "_blank");
 }
 
+function buttonClick3() {
+    window.open("https://open.spotify.com/user/w7ur7ls6wya0583ults6q3l2t?si=50981d68c0344a32");
+}
 
 
 button1.onclick = buttonClick;
 button2.onclick = buttonClick2;
+button3.onclick = buttonClick3;
